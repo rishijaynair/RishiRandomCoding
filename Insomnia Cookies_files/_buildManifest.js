5bc4b5b35b7afedbebf24e12334cc524cc23316c
Rishi Nair <rishijaynair@gmail.com>
2024-01-31 20:55:11 -0500

    Add files via upload

---
+++ Insomnia Cookies_files/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/212-79694c5254500c7f.js","static/chunks/pages/index-77f88babc48cb2e6.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
